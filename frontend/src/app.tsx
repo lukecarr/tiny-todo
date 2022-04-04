8522ffb7c20033574eb7a64168640c2c698b137b
Luke Carr <luke_jcarr@pm.me>
2022-04-04 15:12:19 +0000

    💄 Restyled header/nav to include border

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,11 +1,13 @@
 import Header from 'src/components/Header'
 import Router from 'src/components/Router'
+import Welcome from 'src/components/Welcome'
 
 export function App() {
   return (
-    <div class="container max-w-screen-md min-h-screen py-8">
+    <div class="min-h-screen">
       <Header />
-      <main class="mt-8">
+      <main class="container max-w-screen-md my-8">
+        <Welcome />
         <Router />
       </main>
     </div>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,15 @@
 import type { FunctionalComponent } from 'preact'
-import Welcome from './Welcome'
 
 const Header: FunctionalComponent = () => {
-  return <header>
-    <h1 class="font-extrabold text-3xl">tiny-todo</h1>
-    <Welcome />
-    <nav class="mt-4">
-      <a href="/">todo</a>
-      <span> &#9679; </span>
-      <a href="/about">about</a>
-    </nav>
+  return <header class="border-b-1 border-gray-400 py-8">
+    <div class="container max-w-screen-md">
+      <h1 class="font-extrabold text-3xl">tiny-todo</h1>
+      <nav class="mt-4">
+        <a href="/">todo</a>
+        <span> &#9679; </span>
+        <a href="/about">about</a>
+      </nav>
+    </div>
   </header>
 }
 
