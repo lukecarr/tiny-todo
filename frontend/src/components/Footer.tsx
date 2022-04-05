3dcc846b1f000bc0b43648069b25eb801f413035
Luke Carr <luke_jcarr@pm.me>
2022-04-05 13:50:09 +0000

    💄 Improved styling & alignment of footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -16,14 +16,14 @@ const BuildInfo: FunctionalComponent = () => {
 
   return <div class="text-gray-700 text-sm font-mono text-right">
     <p>{data.version}</p>
-    <p>Build date: {data.date}</p>
-    <p>Commit: {data.commit}</p>
+    <p class="text-xs">Built at: {data.date}</p>
+    <p class="text-xs">Commit: {data.commit}</p>
   </div>
 }
 
 const Footer: FunctionalComponent = () => {
   return <footer class="border-t-1 border-gray-400 py-8">
-    <div class="container max-w-screen-md flex justify-between">
+    <div class="container max-w-screen-md flex justify-between items-center">
       <p>Made with ❤ by <a class="font-semibold underline underline-dotted hover:underline-solid" href="https://github.com/lukecarr" target="_blank" rel="noopener">Luke Carr</a></p>
       <BuildInfo />
     </div>
