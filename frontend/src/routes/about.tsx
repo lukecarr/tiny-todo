d0f42f468e7ae6fad8c019dd709980fdd10bb22c
Luke Carr <luke_jcarr@pm.me>
2022-04-05 13:03:03 +0000

    ✨ Added form for creating new tasks

--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -26,8 +26,9 @@ const About: FunctionalComponent = () => {
         <li class="font-bold">REST API and HTTP server built using Go:</li>
         <ul class="ml-8 mb-4">
           <li>HTTP framework: Fiber.</li>
-          <li>SQL: jmoiron/sqlx + rubenv/sql-migrate.</li>
+          <li>SQL: jmoiron/sqlx, rubenv/sql-migrate, and modernc.org/sqlite.</li>
           <li>CLI framework: spf13/cobra.</li>
+          <li>Logging: zerolog</li>
         </ul>
         <li class="font-bold">Frontend interface built using Preact:</li>
         <ul class="ml-8">
@@ -35,6 +36,8 @@ const About: FunctionalComponent = () => {
           <li>Routing: preact-router and preact-async-route.</li>
           <li>Powered by Vite (bundling for dist and dev server with HMR).</li>
           <li>Styled with WindiCSS.</li>
+          <li>Data fetching and caching: swr and ohmyfetch</li>
+          <li>Form handling: react-hook-form</li>
         </ul>
       </ul>
       
--- frontend/src/routes/todos.tsx
+++ frontend/src/routes/todos.tsx
@@ -1,6 +1,51 @@
+import { $fetch } from 'ohmyfetch'
 import { useEffect } from 'preact/hooks'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import useSWR, { mutate } from 'swr'
 
 import type { FunctionalComponent } from 'preact'
+
+const fetcher = (url: string) => fetch(url).then(r => r.json())
+
+type Task = {
+  id: number
+  name: string
+  complete: boolean
+}
+
+const Tasks: FunctionalComponent = () => {
+  const { data, error } = useSWR<{ tasks: Task[] }>('/api/tasks', fetcher)
+
+  if (error) return <p>Failed to load tasks!</p>
+  if (!data?.tasks) return <p>Loading tasks...</p>
+
+  return <ul>
+    {data.tasks.map((task) => <li key={task.id}>{task.id}: {task.name} {task.complete ? '✅' : '❌'}</li>)}
+  </ul>
+}
+
+type NewTaskInputs = {
+  name: string
+}
+
+const NewTask: FunctionalComponent = () => {
+  const { register, handleSubmit } = useForm<NewTaskInputs>()
+  const create: SubmitHandler<NewTaskInputs> = async ({ name }) => {
+    await $fetch('/api/tasks', {
+      method: 'POST',
+      body: {
+        name,
+      },
+    })
+
+    mutate('/api/tasks')
+  }
+
+  return <form onSubmit={handleSubmit(create)} class="mb-8">
+    <input placeholder="Use tiny-todo everyday!" {...register('name')} />
+    <input type="submit" value="Create task" />
+  </form>
+}
 
 const Todos: FunctionalComponent = () => {
   useEffect(() => {
@@ -9,7 +54,8 @@ const Todos: FunctionalComponent = () => {
   
   return (
     <>
-      <p>You have no tasks left todo!</p>
+      <NewTask />
+      <Tasks />
     </>
   )
 }
