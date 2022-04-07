32a7abb5825c29f99f34c728ff4f1803db015b32
Luke Carr <luke_jcarr@pm.me>
2022-04-07 13:01:57 +0000

    💄 Fixed wrong emoji in frontend header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from './Link'
 const Header: FunctionalComponent = () => {
   return <header border="gray-400 b-1" p="y-8">
     <div container="~" max-w="screen-md">
-      <h1 font="extrabold" text="3xl" flex="~" items="center">👌 tiny-todo</h1>
+      <h1 font="extrabold" text="3xl" flex="~" items="center">📋 tiny-todo</h1>
       <nav m="t-4">
         <Link href="/">todo</Link>
         <span> &#9679; </span>
