c706a4fcff2ad2b7d5f91a0936988f25ade6d32b
Luke Carr <luke_jcarr@pm.me>
2022-04-07 11:20:25 +0000

    💄 Added link styling to nav items

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 import type { FunctionalComponent } from 'preact'
+import Link from './Link'
 
 const Header: FunctionalComponent = () => {
   return <header border="gray-400 b-1" p="y-8">
     <div container="~" max-w="screen-md">
       <h1 font="extrabold" text="3xl" flex="~" items="center">👌 tiny-todo</h1>
       <nav m="t-4">
-        <a href="/">todo</a>
+        <Link href="/">todo</Link>
         <span> &#9679; </span>
-        <a href="/about">about</a>
+        <Link href="/about">about</Link>
       </nav>
     </div>
   </header>
